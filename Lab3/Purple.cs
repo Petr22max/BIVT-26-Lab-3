9a837a7c1c48031e948f3656686fadf80d013ea7
Чайковский Петр <Petor220110@gmail.com>
2026-09-25 12:56:14 +0300

    Update Purple.cs

--- Lab3/Purple.cs
+++ Lab3/Purple.cs
@@ -96,19 +96,9 @@ namespace Lab3
             {
                 attempts++;
                 int x = int.Parse(Console.ReadLine());
-                if (x == -1)
-                {
-                    solution = "Аварийный выход!";
-                    break;
-                }
                 int y = int.Parse(Console.ReadLine());
-                if (y == -1)
-                {
-                    solution = "Аварийный выход!";
-                    break;
-                }
                 int z = int.Parse(Console.ReadLine());
-                if (z == -1)
+                if (x == -1 || y == -1 || z == -1)
                 {
                     solution = "Аварийный выход!";
                     break;
