cbda3c64c9e38442e15b6b700397b94985617c7c
Чайковский Петр <Petor220110@gmail.com>
2026-09-25 12:56:08 +0300

    Update Purple.cs

--- Lab3/Purple.cs
+++ Lab3/Purple.cs
@@ -65,6 +65,25 @@ namespace Lab3
             double avgMark = 0;
 
             // code here
+            for (int i = 0; i < exams; i++)
+{
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+
+    double x = a * 0.4 + b * 0.6;
+
+    if (x > 85)
+        avgMark += 5;
+    else if (x > 70)
+        avgMark += 4;
+    else if (x > 50)
+        avgMark += 3;
+    else
+        avgMark += 2;
+}
+
+if (exams > 0)
+    avgMark /= exams;
 
             // end
 
