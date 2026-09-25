cc19a5a87ef5ef03bdc39c74c9f1373948282bd2
Чайковский Петр <Petor220110@gmail.com>
2026-09-25 13:03:20 +0300

    Update Purple.cs

--- Lab3/Purple.cs
+++ Lab3/Purple.cs
@@ -9,7 +9,16 @@ namespace Lab3
             int count = 0;
 
             // code here
-
+            for (int i = 0; i < n; i++)
+            {
+                double x = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+                double d = Math.Sqrt(x * x + y * y);
+                if (d >= r1 && d <= r2)
+                {
+                    count++;
+                }
+            }
             // end
 
             return count;
@@ -20,7 +29,28 @@ namespace Lab3
             double average = 0;
 
             // code here
-
+            int s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                bool f = false;
+                for (int j = 0; j < 4; j++)
+                {
+                    int x = int.Parse(Console.ReadLine());
+                    s += x;
+                    if (x == 2)
+                    {
+                        f = true;
+                    }
+                }
+                if (f)
+                {
+                    count++;
+                }
+            }
+            if (n > 0)
+            {
+                average = (double)s / (n * 4);
+            }
             // end
 
             return (count, average);
@@ -30,7 +60,22 @@ namespace Lab3
             double avgMark = 0;
 
             // code here
-
+            for (int i = 0; i < exams; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                double x = a * 0.4 + b * 0.6;
+                if (x > 85)
+                    avgMark += 5;
+                else if (x > 70)
+                    avgMark += 4;
+                else if (x > 50)
+                    avgMark += 3;
+                else
+                    avgMark += 2;
+            }
+            if (exams > 0)
+                avgMark /= exams;
             // end
 
             return avgMark;
@@ -41,7 +86,38 @@ namespace Lab3
             int attempts = 0;
 
             // code here
-
+            for (int i = 0; i < limit; i++)
+            {
+                attempts++;
+                int x = int.Parse(Console.ReadLine());
+                if (x == -1)
+                {
+                    solution = "Аварийный выход!";
+                    break;
+                }
+                int y = int.Parse(Console.ReadLine());
+                if (y == -1)
+                {
+                    solution = "Аварийный выход!";
+                    break;
+                }
+                int z = int.Parse(Console.ReadLine());
+                if (z == -1)
+                {
+                    solution = "Аварийный выход!";
+                    break;
+                }
+                int a = x * 100 + y * 10 + z;
+                if (a == code)
+                {
+                    solution = "Доступ разрешен!";
+                    break;
+                }
+            }
+            if (solution == "Код не подобран")
+            {
+                solution = "Система заблокирована!";
+            }
             // end
 
             return (solution, attempts);
@@ -51,7 +127,41 @@ namespace Lab3
             double luck = 0;
 
             // code here
-
+            for (int i = a; i < a + n; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                    case 8:
+                    case 15:
+                    case 22:
+                    case 29:
+                        luck *= 1.5;
+                        if (luck > 100)
+                            luck = 100;
+                        break;
+                    case 4:
+                    case 11:
+                    case 18:
+                    case 25:
+                        luck -= 10;
+                        if (luck < 0)
+                            luck = 0;
+                        break;
+                    case 7:
+                    case 14:
+                    case 21:
+                    case 28:
+                        if (luck < 50)
+                            luck = 55;
+                        break;
+                    default:
+                        luck += 5;
+                        if (luck > 100)
+                            luck = 100;
+                        break;
+                }
+            }
             // end
 
             return luck;
